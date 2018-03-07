3e9fd8b87b30e301ab707028c329a419256f216f
Yusuke AIKO <yusuke.aiko@gmail.com>
2018-03-07 14:56:55 +0900

    minify css

--- functions.php
+++ functions.php
@@ -1,4 +1,14 @@
 <?php
+function  dns_prefetch() {
+  $prefetch = ''; //'<meta http-equiv="x-dns-prefetch-control" content="on">';
+  $domains  = array($_SERVER['SERVER_NAME'], 'cdn.ampproject.org');
+  foreach ($domains as $domain) {
+    $prefetch .= "<link rel=\"dns-prefetch\" href=\"//{$domain}\">";
+  }
+  echo "{$prefetch}\n";
+}
+add_action("wp_head", "dns_prefetch", 0);
+
 function disable_emoji() {
   remove_action('wp_head', 'print_emoji_detection_script', 7);
   remove_action('admin_print_scripts', 'print_emoji_detection_script');
@@ -13,18 +23,34 @@ add_action('init', 'disable_emoji');
 function init_scripts() {
   if (!is_admin()) {
     wp_deregister_script('jquery');
-    // wp_enqueue_script('jquery',   get_template_directory_uri() . '/common/js/jquery-1.11.1.min.js');
   }
 }
 add_action('wp_enqueue_scripts', 'init_scripts');
 
-function  dns_prefetch() {
-  $prefetch = ''; //'<meta http-equiv="x-dns-prefetch-control" content="on">';
-  $domains  = array($_SERVER['SERVER_NAME'], 'cdn.ampproject.org');
-  foreach ($domains as $domain) {
-    $prefetch .= "<link rel=\"dns-prefetch\" href=\"//{$domain}\">";
+function minified_css() {
+  $dir = dirname(__FILE__) . '/';
+  require_once ($dir . 'assets/lib/php-html-css-js-minifier.php');
+  $origin_css    = $dir . 'style.css';
+  $origin_date   = filemtime($origin_css);
+  $minified_css  = $dir . 'style.min.css';
+  $minified_date = filemtime($minified_css);
+  $refresh_flag = false;
+  if (!file_exists($minified_css)) {
+    touch($minified_css);
+    $refresh_flag = true;
   }
-  echo "{$prefetch}\n";
+  if ($minified_date < $origin_date) $refresh_flag = true;
+  if ($refresh_flag) {
+    $css = minify_css(file_get_contents($origin_css));
+    file_put_contents($minified_css, $css);
+  }
+  $inline_css = file_get_contents($minified_css);
+  echo "<style amp-custom>{$inline_css}</style>";
 }
-add_action("wp_head", "dns_prefetch", 0);
+
+/* Navigation */
+register_nav_menus(array(
+  'primary'        => __('グローバルナビゲーション', 'nautilus-code_amp'),
+));
+/* /Navigation */
 ?>
