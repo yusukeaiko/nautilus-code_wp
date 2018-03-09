8a681d5d9ae3325511515923e9f6124fe6acd39b
Yusuke AIKO <yusuke.aiko@gmail.com>
2018-03-09 12:56:57 +0900

    投稿系実装

---
+++ _partial/sns_share_buttons.php
@@ -0,0 +1,7 @@
+<div class="sns_share">
+  <amp-social-share type="gplus"    width="60" height="18"></amp-social-share>
+  <amp-social-share type="twitter"  width="60" height="18"></amp-social-share>
+  <amp-social-share type="facebook" width="60" height="18"></amp-social-share>
+  <amp-social-share type="tumblr"   width="60" height="18"></amp-social-share>
+  <amp-social-share type="system"   width="60" height="18"></amp-social-share>
+</div>
--- single.php
+++ single.php
@@ -1,10 +1,19 @@
 <?php get_header(); ?>
 
-<section>
+<article>
+  <section>
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
-  <time><?php the_date('Y-m-d h:m:s'); ?></time>
-  <?php the_content(); ?>
+    <dl class="post_meta">
+      <dt><i class="fa fa-clock-o" aria-hidden="true"></i></dt><dd><time><?php the_date('Y-m-d h:m:s'); ?></time></dd>
+      <?php if (get_the_category()): ?>
+        <dt><i class="fa fa-folder" aria-hidden="true"></i></dt><dd><?php the_category(', ', 'multiple'); ?></dd>
+      <?php endif; ?>
+      <?php if (get_the_tags()) the_tags('<dt><i class="fa fa-tags" aria-hidden="true"></i></dt><dd>', ', ' ,'</dd>'); ?>
+    </dl>
+    <?php the_content(); ?>
 <?php endwhile; wp_reset_postdata(); endif; ?>
-</section>
+  <?php get_template_part('_partial/sns_share_buttons'); ?>
+  </section>
+</article>
 
 <?php get_footer(); ?>
